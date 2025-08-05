f46dc830420dc69fdfe85f39e9c9f95013f58c2d
Admin <romanpluta42@gmail.com>
2025-08-05 16:02:30 +0300

    add task solution

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,15 +1,14 @@
-import usersFromServer from '../../api/users.json';
 import { UserInfo } from '../UserInfo/UserInfo';
 
 export const TodoInfo = ({ todo }) => {
   // eslint-disable-next-line no-shadow
-  const user = usersFromServer.find(user => user.id === todo.userId);
+  const { user } = todo;
 
   return (
     <article className="TodoInfo TodoInfo--completed">
       <h2 className="TodoInfo__title">{todo.title}</h2>
 
-      {user && <UserInfo key={user.id} user={user} />}
+      {user && <UserInfo key={user.id} user={todo.user} />}
     </article>
   );
 };
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,9 +1,8 @@
-import todosFromServer from '../../api/todos.json';
 import { TodoInfo } from '../TodoInfo/TodoInfo';
 
-export const TodoList = () => (
+export const TodoList = ({ todos }) => (
   <section className="TodoList">
-    {todosFromServer.map(todo => (
+    {todos.map(todo => (
       <TodoInfo key={todo.id} todo={todo} />
     ))}
   </section>
