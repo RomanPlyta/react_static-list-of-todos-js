82a0b8285cda4743edf0f48b22b7ed80d7b4c11e
Admin <romanpluta42@gmail.com>
2025-08-05 15:02:46 +0300

    add task solutionv1

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,16 @@
-// Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+import todosFromServer from '../../api/todos.json';
+import { UserInfo } from '../UserInfo/UserInfo';
+
+export const TodoInfo = ({ user }) => (
+  <article className="TodoInfo TodoInfo--completed">
+    {todosFromServer
+      .filter(todo => todo.userId === user.id)
+      .map(todo => (
+        <h2 key={todo.id} className="TodoInfo__title">
+          {todo.title}
+        </h2>
+      ))}
+
+    <UserInfo user={user} />
+  </article>
+);
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,2 +1,10 @@
-// Add the required props
-export const TodoList = () => <>TodoList markup</>;
+import usersFromServer from '../../api/users.json';
+import { TodoInfo } from '../TodoInfo/TodoInfo';
+
+export const TodoList = () => (
+  <section className="TodoList">
+    {usersFromServer.map(user => (
+      <TodoInfo key={user.id} user={user} />
+    ))}
+  </section>
+);
